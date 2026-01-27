ef0060980b001969f7a37c649f496eceff99dcf1
akanksha-th <ak.t.kree@gmail.com>
2026-01-27 17:44:04 +0530

    Update dictionary notes

--- pages/4_Dictionary.py
+++ pages/4_Dictionary.py
@@ -204,6 +204,146 @@ This structure is how:
 st.divider()
 
 st.markdown("""
+### Advanced Dictionary Operations
+            
+- **setdefault()** –>   
+        - if the key exists, it returns the existing value and doesn't change the dictionary
+        - if the key does not exist, it inserts the new key with the default value and returns the default value
+        - another use case for setdefault() is grouping items in a dictionary where each key's value is a list (or other mutable type)
+""")
+code_setdefault = '''
+d = {"name": "Tom Riddle"}
+name = d.setdefault("name", "Voldemort")
+print(f"Name: {name}")
+print(f"Dictionary: {d}")
+
+age = d.setdefault("age", "21")
+print(f"Age: {age}")
+print(f"Dictionary: {d}")
+
+d.setdefault("roll_number", []).append(103)
+d.setdefault("roll_number", []).append(221)
+print(f"Dictionary: {d}")
+
+demo_data = [("a", 1), ("b", 2), ("a", 3), ("c", 3)]
+grouped_data = {}
+for k, v in demo_data:
+    grouped_data.setdefault(k, []).append(v)
+print(f"Grouped Data: {grouped_data}")
+'''
+st.code(code_setdefault, language="python")
+st.markdown("""
+Output:  
+            `Name: Tom Riddle` 
+            `Dictionary: {'name': 'Tom Riddle'}`   
+
+            `Age: 21`
+            `Dictionary: {'name': 'Tom Riddle', 'age': '21'}`   
+
+            `Dictionary: {'name': 'Tom Riddle', 'age': '21', 'roll_number': [103, 221]}`   
+
+            `Grouped Data: {'a': [1, 3], 'b': [2], 'c': [3]}`   
+""")
+st.info("""
+### setdefault() vs get() 
+
+*_setdefault()_* modifies the dictionary by adding the key-value pair if the key is missing,   
+while *_get()_* only returns a default value without modifying the dictionary        
+""")
+
+st.markdown("""
+- ** frequency map ** –>   
+        - using .get() function   
+        - using collections.Counter –> specifically designed for counting hashable objects   
+        - using collections.defaultdict –> automatically initializes new keys with a default value (e.g., 0 for an int) when they are first accessed, eliminating the need to check if a key exists   
+        - using explicit checking   
+""")
+code_freq_map = '''
+fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'banana']
+
+# method 1: .get()
+freq = {}
+for fruit in fruits:
+    freq[fruit] = freq.get(fruit, 0) + 1
+print(f"Freq Map: {freq}")
+
+# method 2
+from collections import Counter
+freq_map = Counter(fruits)
+print(freq_map)
+print(f"Freq Map: {dict(freq_map)}")
+
+# method 3
+from collections import defaultdict
+freq_map = defaultdict(int)
+for fruit in fruits:
+    freq_map[fruit] += 1
+print(f"Freq Map: {freq_map}")
+
+# method 4: explicit checking
+freq_map = {}
+for fruit in fruits:
+    if fruit in freq_map:
+        freq_map[fruit] += 1
+    else:
+        freq_map[fruit] = 1
+print(f"Freq Map: {freq}")
+'''
+st.code(code_freq_map, language="python")
+
+st.markdown("""
+Output:   
+        `Freq Map: {'apple': 2, 'banana': 3, 'orange': 1}`   
+            
+        `Counter({'banana': 3, 'apple': 2, 'orange': 1})`   
+        `Freq Map: {'apple': 2, 'banana': 3, 'orange': 1}`   
+            
+        `Freq Map: defaultdict(<class 'int'>, {'apple': 2, 'banana': 3, 'orange': 1})`   
+            
+        `Freq Map: {'apple': 2, 'banana': 3, 'orange': 1}`   
+
+--- 
+
+- ** Dictionary Merging ** –>   
+        - update()   
+        - **|** (Union) operator –> cleaner than update() in Python   
+        
+""")
+code_dict_merge = '''
+dict1 = {"a": 1, "b": 2}
+dict2 = {"b": 1, "c": 4}
+
+# method 1
+merged_dict = dict1 | dict2
+print(merged_dict)
+
+# method 2
+merged_dict = {**dict1, **dict2}
+print(merged_dict)
+
+# updating the existing dictionary in-place
+dict1.update(dict2)
+print(dict1)
+
+# OR
+dict1 = {"a": 1, "b": 2}
+dict2 |= dict1
+print(dict2)
+'''
+st.code(code_dict_merge, language="python")
+st.markdown("""
+Output:   
+        `{'a': 1, 'b': 1, 'c': 4}`   
+
+        `{'a': 1, 'b': 1, 'c': 4}`   
+
+        `{'a': 1, 'b': 1, 'c': 4}`   
+            
+        `{'b': 2, 'c': 4, 'a': 1}`
+""")
+
+st.divider()
+st.markdown("""
 ### Some Important Points to Remember about Dictionaries
             
 Dictionaries exist to solve a different kind of problem than lists. While lists are good for storing ordered collections and accessing elements by position, real-world problems require fast lookup based on a key.
